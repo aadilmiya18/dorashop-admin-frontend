4891b907da42213e49cdc7f9c8b7d66871a4b0c7
Aadil <miyaaadil99@gmail.com>
2025-10-19 14:54:07 +0545

    Initialize the project

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+      <div class="tw-bg-blue-500 tw-text-white tw-p-6 tw-rounded-xl">
+        Hello Tailwind + Quasar 🎉
+      </div>
+
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
